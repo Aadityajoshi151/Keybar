25f0806b01295c9e9e1f3120165e8c130b5206d9
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-07-29 20:16:22 +0530

    variable name changed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-const SEGMENTS = 15;
+const TOTAL_SEGMENTS = 15;
 const COLORS = ['blue','red','green'];
 
 //TODO:- Fix ghosting issue
 
 document.addEventListener("keypress",async function(){
     var color = COLORS[Math.floor(Math.random()  * COLORS.length)];
-    for(let i=SEGMENTS; i>=1 ;i--){
+    for(let i=TOTAL_SEGMENTS; i>=1 ;i--){
         document.getElementById(i).style.color = color;
         await sleep(10);
     }
-    for(let j=1; j<=SEGMENTS; j++){
+    for(let j=1; j<=TOTAL_SEGMENTS; j++){
         document.getElementById(j).style.color = 'white';
         await sleep(20);
     }
