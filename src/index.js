8fb269cd92c2eedc95429750a9ef472e08881569
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-07-29 20:05:01 +0530

    random color added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-const SEGMENTS = 10;
+const SEGMENTS = 15;
+const COLORS = ['blue','red','green'];
+
+//TODO:- Fix ghosting issue
+
 document.addEventListener("keypress",async function(){
+    var color = COLORS[Math.floor(Math.random()  * COLORS.length)];
     for(let i=SEGMENTS; i>=1 ;i--){
-        document.getElementById(i).style.color = 'green';
-        await sleep(20);
+        document.getElementById(i).style.color = color;
+        await sleep(10);
     }
     for(let j=1; j<=SEGMENTS; j++){
         document.getElementById(j).style.color = 'white';
-        await sleep(40);
+        await sleep(20);
     }
 })
