5dad275a8302139c01c7623ee6a6b506f3c4d119
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-07-30 10:56:57 +0530

    random height added

--- src/index.js
+++ src/index.js
@@ -5,22 +5,20 @@ const TOTAL_SEGMENTS = 15;
 const COLORS = ['blue','red','green'];
 flag=true;
 
-//TODO:- Fix ghosting issue
-
 document.addEventListener("keypress",async function(){
     if (flag){
         flag=false;
         var color = COLORS[Math.floor(Math.random()  * COLORS.length)];
-        for(let i=TOTAL_SEGMENTS; i>=1 ;i--){
+        HEIGHT = Math.floor((Math.random() * TOTAL_SEGMENTS)+1);
+        for(let i=TOTAL_SEGMENTS; i>=HEIGHT ;i--){
             document.getElementById(i).style.color = color;
             await sleep(10);
         }   
-        for(let j=1; j<=TOTAL_SEGMENTS; j++){
+        for(let j=HEIGHT; j<=TOTAL_SEGMENTS; j++){
             document.getElementById(j).style.color = 'white';
             await sleep(20);
         }
         flag=true;
-    }
-    
+    }   
 })
 
