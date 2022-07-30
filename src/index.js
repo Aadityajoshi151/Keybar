0c5f0e0d162c8863ec361ad4005b93ff109bb826
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-07-30 10:46:03 +0530

    fix ghosting issue

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ function sleep(ms) {
 }
 const TOTAL_SEGMENTS = 15;
 const COLORS = ['blue','red','green'];
+flag=true;
 
 //TODO:- Fix ghosting issue
 
 document.addEventListener("keypress",async function(){
-    var color = COLORS[Math.floor(Math.random()  * COLORS.length)];
-    for(let i=TOTAL_SEGMENTS; i>=1 ;i--){
-        document.getElementById(i).style.color = color;
-        await sleep(10);
+    if (flag){
+        flag=false;
+        var color = COLORS[Math.floor(Math.random()  * COLORS.length)];
+        for(let i=TOTAL_SEGMENTS; i>=1 ;i--){
+            document.getElementById(i).style.color = color;
+            await sleep(10);
+        }   
+        for(let j=1; j<=TOTAL_SEGMENTS; j++){
+            document.getElementById(j).style.color = 'white';
+            await sleep(20);
+        }
+        flag=true;
     }
-    for(let j=1; j<=TOTAL_SEGMENTS; j++){
-        document.getElementById(j).style.color = 'white';
-        await sleep(20);
-    }
+    
 })
+
