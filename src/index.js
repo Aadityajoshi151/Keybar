79a120e3bc31dfe75c60ac885f6de2beb648a2ad
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-07-31 10:47:09 +0530

    create segments

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ document.addEventListener("keypress",async function(){
         var color = COLORS[Math.floor(Math.random()  * COLORS.length)];
         HEIGHT = Math.floor((Math.random() * TOTAL_SEGMENTS)+1);
         for(let i=TOTAL_SEGMENTS; i>=HEIGHT ;i--){
-            document.getElementById(i).style.color = color;
+            document.getElementById(i).style.backgroundColor = color;
             await sleep(10);
         }   
         for(let j=HEIGHT; j<=TOTAL_SEGMENTS; j++){
-            document.getElementById(j).style.color = 'white';
+            document.getElementById(j).style.backgroundColor = 'white';
             await sleep(20);
         }
         flag=true;
