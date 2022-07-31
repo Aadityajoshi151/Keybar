424b0ad8cf12ae64a329356899ca512af00711e6
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-07-31 10:59:52 +0530

    create elements dynamically

--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ const TOTAL_SEGMENTS = 16;
 const COLORS = ['blue','red','lime','fuchsia','violet','orange'];
 flag=true;
 
+//Creating the segments dynamically via js
+for(let i=1 ; i<=TOTAL_SEGMENTS ; i++){
+    const segment = document.createElement('div');
+    segment.classList.add('segment');  
+    segment.setAttribute('id', i);
+    document.getElementById('container').appendChild(segment);
+}
+
 document.addEventListener("keypress",async function(){
     if (flag){
         flag=false;
         var color = COLORS[Math.floor(Math.random()  * COLORS.length)];
         HEIGHT = Math.floor((Math.random() * TOTAL_SEGMENTS)+1);
-        for(let i=TOTAL_SEGMENTS; i>=HEIGHT ;i--){
+        for(let i=TOTAL_SEGMENTS ; i>=HEIGHT ; i--){
             document.getElementById(i).style.backgroundColor = color;
             await sleep(10);
         }   
-        for(let j=HEIGHT; j<=TOTAL_SEGMENTS; j++){
+        for(let j=HEIGHT ; j<=TOTAL_SEGMENTS ; j++){
             document.getElementById(j).style.backgroundColor = 'white';
             await sleep(20);
         }
