3fe35fff8ceae79aae1c1859bd098d47eb38c6dd
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-08-05 17:46:23 +0530

    add code for riseup mode

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const TOTAL_SEGMENTS = 27;
 
 const COLORS = ['#f90020','#e500bd','#d101f7','#ac0fff','#7a29ff','#394eff','#076afc','#0d71fd','#0798db','#05b99e','#05df57','#0df91d','#80fd01','#b6f802','#dcf001','#fbee02'];
 
+const four_shades = ['#02f412','#1ad826','#1daf27','#1a8220']
+
 var MODE = "randomHC" //default mode
 var flag=true;
 
@@ -37,7 +39,28 @@ document.addEventListener("keyup",async function(){
             }
         break
         case "riseUp":
-            console.log("RISE!!!")
+            for(let i=TOTAL_SEGMENTS; i>=1; i--){
+                console.log(i)
+                document.getElementById(i).style.backgroundColor = four_shades[0]
+                await sleep(12);
+                if((i+1) <= TOTAL_SEGMENTS){
+                    document.getElementById(i+1).style.backgroundColor = four_shades[1]
+                }
+                await sleep(12);
+                if((i+2) <= TOTAL_SEGMENTS){
+                    document.getElementById(i+2).style.backgroundColor = four_shades[2]
+                }
+                await sleep(12);
+                if((i+3) <= TOTAL_SEGMENTS){
+                    document.getElementById(i+3).style.backgroundColor = four_shades[3]
+                }
+                await sleep(12);
+                if((i+4) <= TOTAL_SEGMENTS){
+                    document.getElementById(i+4).style.backgroundColor = 'white';
+                          
+                }
+                await sleep(12);
+            }
         break
     }
        
